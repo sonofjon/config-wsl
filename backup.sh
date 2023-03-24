6c736833417e13bdf8f679c941d4963eada83642
Andreas Jonsson <ajdev8@gmail.com>
2023-03-24 16:47:28 +0100

    Quote variable to handle spaces in $WINUSER

--- backup.sh
+++ backup.sh
@@ -19,7 +19,7 @@
 
 WINUSER=$(powershell.exe '$env:UserName' | tr -d '\r')
 SOURCE=$HOME/
-DEST=/mnt/c/Users/$WINUSER/Backup/wsl/
+DEST=/mnt/c/Users/"$WINUSER"/Backup/wsl/
 EXCLUDE_FILE=$HOME/dotfiles/config-wsl/exclude.rsync
 
 rsync -a --delete $@ --exclude-from=$EXCLUDE_FILE "$SOURCE" "$DEST"
