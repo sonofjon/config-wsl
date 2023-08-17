e7ae22be54b10c4cdea6fa35aa7ac9857dc23400
Andreas Jonsson <ajdev8@gmail.com>
2023-08-17 09:51:48 +0200

    Add script that kills WSL
    This may be required if WSL becomes unresponsive after sleep.

---
+++ Script/Kill WSL.bat
@@ -0,0 +1,11 @@
+rem Run as Administrator
+rem
+rem Reference: https://github.com/microsoft/WSL/issues/8696
+
+echo "Listing all wsl tasks"
+tasklist /M wsl*
+
+echo "Sending kill to wslservice"
+taskkill /IM wslservice.exe /F
+
+echo "Done"
