a6275b85af1d9ae071aeffc41bc91a84db31ba69
Andreas Jonsson <ajdev8@gmail.com>
2023-08-31 10:20:14 +0200

    Make output less verbose

--- Script/Kill WSL.bat
+++ Script/Kill WSL.bat
@@ -1,11 +1,11 @@
-rem Run as Administrator
-rem
-rem Reference: https://github.com/microsoft/WSL/issues/8696
+@rem Run as Administrator
+@rem
+@rem Reference: https://github.com/microsoft/WSL/issues/8696
 
-echo "Listing all wsl tasks"
-tasklist /M wsl*
+@echo Listing all wsl tasks:
+@tasklist /M wsl*
 
-echo "Sending kill to wslservice"
-taskkill /IM wslservice.exe /F
+@echo Sending kill to wslservice:
+@taskkill /IM wslservice.exe /F
 
-echo "Done"
+@echo Done!
